6bfec2d739746da1e2bd663bf12782db88ecdb76
Ravi Soares <broccosoaresravi@gmail.com>
2026-05-06 19:29:01 -0300

    style change

--- frontend/src/pages/Estoque/EstoqueCard.jsx
+++ frontend/src/pages/Estoque/EstoqueCard.jsx
@@ -6,17 +6,17 @@ const EstoqueCard = ({ item, onClick }) => {
   
   return (
   
-  <div className={styles.card}>
+  <div className={styles.card} onClick={onClick}>
     
     <div className={styles.imageContainer}>
-      <img src={item.produtoFoto || 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ed/Item_sem_imagem.svg/1280px-Item_sem_imagem.svg.png'}  alt={item.produtoNome} />
+      <img src={item.produtoFoto || 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ed/Item_sem_imagem.svg/1280px-Item_sem_imagem.svg.png'} alt={item.produtoNome} />
       <button className={styles.menuButton} onClick={(e) => { e.stopPropagation(); onClick(); }}> <Ellipsis size={18} /> </button>
     </div>
           
     <div className={styles.info}>
       
       <h3 className={styles.nome}>{item.produtoNome}</h3>
-      <p className={styles.valorUnitario}>Unitário: R$ {item.produtoPreco?.toFixed(2)}</p>
+      <p className={styles.valorUnitario}>Valor unitário: R$ {item.produtoPreco?.toFixed(2)}</p>
         
       <div className={styles.estoqueBadge}>
         <span className={styles.qtdLabel}>Em estoque:</span>
@@ -24,7 +24,7 @@ const EstoqueCard = ({ item, onClick }) => {
       </div>
 
     </div>
-    
+
   </div>
   );
 };
